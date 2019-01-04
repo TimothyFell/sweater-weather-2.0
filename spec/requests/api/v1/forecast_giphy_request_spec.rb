dde6e6b3ee4b88dc9e2ff88f8586ef2dec12020b
TimothyFell <timothy.fell846@gmail.com>
2019-01-04 09:20:16 -0700

    Adds test for new gif endpoint to have a successful response

---
+++ spec/requests/api/v1/forecast_giphy_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'Weather for a City with Gifs', :type => :request do
+
+  describe 'GET /api/v1/gifs?location=denver,co' do
+
+    before(:each) do
+      stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=denver,co&key=#{ENV['GEOCODING_API_KEY']}").to_return(body: File.read("./spec/fixtures/geocode_response.json"))
+
+      stub_request(:get, "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/39.7392358,-104.990251").to_return(body: File.read("./spec/fixtures/dark_sky_response.json"))
+    end
+
+    it 'should have a successful response' do
+      get '/api/v1/gifs?location=denver,co'
+
+      expect(response).to be_successful
+    end
+
+  end
+
+end
