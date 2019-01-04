4dd31045b925d126dbb538fe42db4e27e29f9e63
TimothyFell <timothy.fell846@gmail.com>
2019-01-03 18:09:38 -0700

    Added controller and stubs to get forecast request to return a status of successful

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    # coords = GoogleService.new.get_lat_lon(params[:location])
-    # binding.pry
-    # DarkSkyService.new.get_forecast(lat,lng)
+    coords = GoogleService.new.get_lat_lon(params[:location])
+    DarkSkyService.new.get_forecast(coords[:lat],coords[:lng])
   end
 end
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -5,10 +5,13 @@ describe 'Weather for a City' do
   describe 'GET /api/v1/forecast?location=denver,co' do
 
     before(:each) do
-      get '/api/v1/forecast?location=denver,co'
+      stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=denver,co&key=#{ENV['GEOCODING_API_KEY']}").to_return(body: File.read("./spec/fixtures/geocode_response.json"))
+
+      stub_request(:get, "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/39.7392358,-104.990251").to_return(body: File.read("./spec/fixtures/dark_sky_response.json"))
     end
 
     it 'should return current weather as a json object' do
+      get '/api/v1/forecast?location=denver,co'
       expect(response).to be_successful
     end
 
