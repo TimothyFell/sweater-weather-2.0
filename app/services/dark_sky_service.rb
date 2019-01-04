6ff77143ce46098d2f2271855f5311baf24b7da9
TimothyFell <timothy.fell846@gmail.com>
2019-01-03 17:34:45 -0700

    Add dark sky service with tests and vcr

---
+++ app/services/dark_sky_service.rb
@@ -0,0 +1,18 @@
+class DarkSkyService
+
+  def get_forecast(lat,lng)
+    get_json("forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{lng}")
+  end
+
+  private
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://api.darksky.net/")
+  end
+
+end
