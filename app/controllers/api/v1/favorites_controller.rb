10a3b15ab895e182a152aeebb73ee120b416c5b9
TimothyFell <timothy.fell846@gmail.com>
2019-01-07 14:25:59 -0700

    Add favorites request and storing using TDD

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::FavoritesController < ApplicationController
+
+  def create
+    user = User.find_by(api_key: favorite_params["api_key"])
+
+    if user
+      favorite = Favorite.find_or_create_by(location: clean_location)
+      user.user_favorites.create(favorite_id: favorite.id)
+      render json:'Success',status:204
+    else
+      render json:'Unauthorized',status:401
+    end
+  end
+
+  private
+
+  def clean_location
+    favorite_params["location"].downcase.strip.delete(',').split.join(',')
+  end
+
+  def favorite_params
+    params.permit("location","api_key")
+  end
+
+end
---
+++ spec/requests/api/v1/user_favorite_request_spec.rb
@@ -0,0 +1,110 @@
+require 'rails_helper'
+
+describe 'User Favorites Locations' do
+
+  describe 'POST /api/v1/favorites' do
+
+    describe 'positive path' do
+
+      before(:each) do
+        @user_1 = User.create({
+          "email"=>"whatever@example.com",
+          "password"=>"password",
+          "password_confirmation"=>"password",
+          "api_key"=>"potato"
+        })
+        @user_2 = User.create({
+          "email"=>"bob@example.com",
+          "password"=>"password",
+          "password_confirmation"=>"password",
+          "api_key"=>"carrot"
+        })
+      end
+
+      it 'should be successful' do
+        headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+        post "/api/v1/favorites", :params =>
+          {"location"=>"Denver, CO",
+           "api_key"=>"potato",
+           "controller"=>"api/v1/favorites",
+           "action"=>"create", "favorite"=>{"location"=>"Denver, CO",
+            "api_key"=>"potato"}}, :headers => headers
+
+        expect(response).to be_successful
+      end
+
+      it 'should create a favorite for the user' do
+        headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+        post "/api/v1/favorites", :params =>
+          {"location"=>"Denver, CO",
+           "api_key"=>"potato",
+           "controller"=>"api/v1/favorites",
+           "action"=>"create", "favorite"=>{"location"=>"Denver, CO",
+            "api_key"=>"potato"}}, :headers => headers
+
+        expect(Favorite.last.location).to eq("denver,co")
+      end
+
+      it 'different users can have the same favorite' do
+        headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+        post "/api/v1/favorites", :params =>
+          {"location"=>"Denver, CO",
+           "api_key"=>"potato",
+           "controller"=>"api/v1/favorites",
+           "action"=>"create", "favorite"=>{"location"=>"Denver, CO",
+            "api_key"=>"potato"}}, :headers => headers
+
+        headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+        post "/api/v1/favorites", :params =>
+          {"location"=>"Denver, CO",
+           "api_key"=>"carrot",
+           "controller"=>"api/v1/favorites",
+           "action"=>"create", "favorite"=>{"location"=>"Denver co",
+            "api_key"=>"carrot"}}, :headers => headers
+
+        expect(UserFavorite.all.count).to eq(2)
+        expect(UserFavorite.first.user_id).to eq(@user_1.id)
+        expect(UserFavorite.last.user_id).to eq(@user_2.id)
+        expect(Favorite.all.count).to eq(1)
+      end
+
+      describe 'if the api_key is incorrect' do
+        it 'it should return 401 error' do
+          headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+          post "/api/v1/favorites", :params =>
+            {"location"=>"Denver, CO",
+             "api_key"=>"george",
+             "controller"=>"api/v1/favorites",
+             "action"=>"create", "favorite"=>{"location"=>"Denver, CO",
+              "api_key"=>"george"}}, :headers => headers
+
+          expect(response).to_not be_successful
+        end
+      end
+
+    end
+
+    describe 'if the user inst in the database' do
+
+      it 'should return a 401 error' do
+        headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+        post "/api/v1/favorites", :params =>
+          {"lcoation"=>"Peanut, PA",
+           "api_key"=>"joe",
+           "controller"=>"api/v1/favorites",
+           "action"=>"create", "favorite"=>{"lcoation"=>"Peanut, PA", "api_key"=>"joe"}}, :headers => headers
+
+        expect(response).to_not be_successful
+      end
+
+    end
+
+  end
+
+end
