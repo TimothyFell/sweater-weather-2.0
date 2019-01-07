c12ad78e18d31fece5df6f889111debf7e01d139
TimothyFell <timothy.fell846@gmail.com>
2019-01-07 07:27:50 -0700

    Add sad path for users who dont exist trying to login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,9 +2,12 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: session_params["email"])
-    session['user_id'] = user.id
-    response = { "api_key"=>"#{user.api_key}" }
-    render json: response
+    if User.find_by(email: session_params["email"])
+      session['user_id'] = user.id
+      render json:{ "api_key"=>"#{user.api_key}" }
+    else
+      render json:'Error',status:401
+    end
   end
 
   private
--- spec/requests/api/v1/user_can_request_login_spec.rb
+++ spec/requests/api/v1/user_can_request_login_spec.rb
@@ -4,32 +4,52 @@ describe 'User Login Request' do
 
   describe 'POST /api/v1/sessions' do
 
-    before(:each) do
-      user = User.create({
-        "email"=>"whatever@example.com",
-        "password"=>"password",
-        "password_confirmation"=>"password",
-        "api_key"=>"potato"
-      })
+    describe 'positive path' do
 
-      headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+      before(:each) do
+        user = User.create({
+          "email"=>"whatever@example.com",
+          "password"=>"password",
+          "password_confirmation"=>"password",
+          "api_key"=>"potato"
+        })
 
-      post "/api/v1/sessions", :params =>
-        {"email"=>"whatever@example.com",
-         "password"=>"password",
-         "controller"=>"api/v1/sessions",
-         "action"=>"create", "session"=>{"email"=>"whatever@example.com", "password"=>"password"}}, :headers => headers
+        headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+        post "/api/v1/sessions", :params =>
+          {"email"=>"whatever@example.com",
+           "password"=>"password",
+           "controller"=>"api/v1/sessions",
+           "action"=>"create", "session"=>{"email"=>"whatever@example.com", "password"=>"password"}}, :headers => headers
+      end
+
+      it 'should be successful' do
+        expect(response).to be_successful
+      end
+
+      it 'should return the users api_key' do
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response).to be_a(Hash)
+        expect(parsed_response).to have_key("api_key")
+        expect(parsed_response).to have_value("potato")
+      end
+
     end
 
-    it 'should be successful' do
-      expect(response).to be_successful
-    end
+    describe 'if the user inst in the database' do
 
-    it 'should return the users api_key' do
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response).to be_a(Hash)
-      expect(parsed_response).to have_key("api_key")
-      expect(parsed_response).to have_value("potato")
+      it 'should return a 401 error' do
+        headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+        post "/api/v1/sessions", :params =>
+          {"email"=>"whatever@example.com",
+           "password"=>"password",
+           "controller"=>"api/v1/sessions",
+           "action"=>"create", "session"=>{"email"=>"whatever@example.com", "password"=>"password"}}, :headers => headers
+
+        expect(response).to_not be_successful
+      end
+
     end
 
   end
