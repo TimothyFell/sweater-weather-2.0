0d04a6fc5678d6c8c5fd21b1ae92eda3ec611c29
TimothyFell <timothy.fell846@gmail.com>
2019-01-08 07:15:43 -0700

    Add sad path tests and authentication

--- spec/requests/api/v1/user_can_request_login_spec.rb
+++ spec/requests/api/v1/user_can_request_login_spec.rb
@@ -36,16 +36,49 @@ describe 'User Login Request' do
 
     end
 
-    describe 'if the user inst in the database' do
+    describe 'Sad Paths' do
 
-      it 'should return a 401 error' do
+      before(:each) do
+        user = User.create({
+          "email"=>"whatever@example.com",
+          "password"=>"password",
+          "password_confirmation"=>"password",
+          "api_key"=>"potato"
+        })
+      end
+
+      it 'Not in Database: 401 error' do
+        headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+        post "/api/v1/sessions", :params =>
+          {"email"=>"joe@example.com",
+           "password"=>"wakawaka",
+           "controller"=>"api/v1/sessions",
+           "action"=>"create", "session"=>{"email"=>"joe@example.com", "password"=>"wakawaka"}}, :headers => headers
+
+        expect(response).to_not be_successful
+      end
+
+      it 'Incorrect Password: 401 Error' do
         headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
 
         post "/api/v1/sessions", :params =>
           {"email"=>"whatever@example.com",
+           "password"=>"wakawaka",
+           "controller"=>"api/v1/sessions",
+           "action"=>"create", "session"=>{"email"=>"whatever@example.com", "password"=>"wakawaka"}}, :headers => headers
+
+        expect(response).to_not be_successful
+      end
+
+      it 'Incorrect Email: 401 Error' do
+        headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+
+        post "/api/v1/sessions", :params =>
+          {"email"=>"joe@example.com",
            "password"=>"password",
            "controller"=>"api/v1/sessions",
-           "action"=>"create", "session"=>{"email"=>"whatever@example.com", "password"=>"password"}}, :headers => headers
+           "action"=>"create", "session"=>{"email"=>"joe@example.com", "password"=>"password"}}, :headers => headers
 
         expect(response).to_not be_successful
       end
